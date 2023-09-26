7ca8fe86567f46154935233f56340abb4ae01c5f
Darren Bono <dbono215@gmail.com>
2023-09-26 16:51:16 +0200

    Update README, linting

--- supernets.py
+++ supernets.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python
 """Docstring missing."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
-from collections import defaultdict
-import ipaddress
 import argparse
+import ipaddress
 import sys
+from collections import defaultdict
 
-
-# define globals
-networks = dict()
+networks = {}
 prefixes = defaultdict(list)
-verbose_output = False
+verbose_output = True
 
 
 def verbose_print(*args, **kwargs):
@@ -23,53 +15,32 @@ def verbose_print(*args, **kwargs):
         output = ' '.join(args)
         print(output)
 
-
-def add_network(*args):
-    """ Adds network(s) to the global networks dictionary.
-    Since network is a key value, duplicates are inherently removed.
-    """
-    global networks
-    for network in args:
-        if network not in networks:
-            networks[network] = network.prefixlen
-            add_network_to_prefixes(network)
-
+def add_network(network):
+    if network not in networks:
+        networks[network] = network.prefixlen
+        add_network_to_prefixes(network)
 
 def delete_network(*args):
-    """Removes one of more networks from the global networks dictionary."""
-    global networks
     for network in args:
         networks.pop(network, None)
 
-
 def add_network_to_prefixes(network):
-    """ Adds networks to the prefix dictionary.
-    The prefix dictionary is keyed by prefixes.
-    Networks of the same prefix length are stored in a list.
-    """
-    global prefixes
-    prefix = network.prefixlen
-    prefixes[prefix].append(network)
-
+    prefixes[network.prefixlen].append(network)
 
 def process_input(subnets):
-    """ Read each network from file and compare to the current supernet."""
     try:
         with open(subnets) as data:
             for net in data:
                 try:
-                    network = ipaddress.ip_network(
-                        net.strip().encode().decode(), strict=False)
+                    network = ipaddress.ip_network(net.strip().encode().decode(), strict=False)
                     add_network(network)
                 except ValueError:
-                    print('!!!', net, ' is not a valid network')
+                    print(f"{net} is not a valid network")
     except IOError:
         print("File not found, goodbye!")
         sys.exit(1)
 
-
 def process_prefixes(prefix=0):
-    """Read each list of networks starting with the largest prefixes."""
     global prefixes
     if prefix < 128:
         process_prefixes(prefix + 1)
@@ -77,35 +48,35 @@ def process_prefixes(prefix=0):
         verbose_print("="*79, "\nPrefix Length = %s" % (prefix))
         compare_networks_of_same_prefix_length(sorted(prefixes[prefix]))
 
-
 def compare_networks_of_same_prefix_length(prefix_list):
     previous_net = None
+
     for current_net in prefix_list:
-        existing_supernet = find_existing_supernet(current_net)
-        if existing_supernet:
+        if existing_supernet := find_existing_supernet(current_net):
             delete_network(current_net)
-            verbose_print("%s found in %s" % (current_net, existing_supernet))
+            verbose_print(f"{current_net} found in {existing_supernet}")
+
         elif previous_net is None:
             previous_net = current_net
+
         else:
-            # Calculate a one bit larger subet and see if they are the same.
             supernet1 = previous_net.supernet(prefixlen_diff=1)
             supernet2 = current_net.supernet(prefixlen_diff=1)
+
             if supernet1 == supernet2:
                 add_network(supernet1)
                 delete_network(previous_net, current_net)
-                verbose_print("%s and %s aggregate to %s" % (
-                    previous_net, current_net, supernet1))
+                verbose_print(f"{previous_net} and {current_net} aggregate to {supernet1}")
                 previous_net = None
-            else:
-                verbose_print("%s is unique" % (previous_net))
-                previous_net = current_net
-    if previous_net is not None:
-        verbose_print("%s is unique" % (previous_net))
 
+            else:
+                verbose_print(f"{previous_net} is unique")
+                previous_net = current_net
+
+    if previous_net is not None:
+        verbose_print(f"{previous_net} is unique")
 
 def find_existing_supernet(network):
-    """ This function checks if a subnet is part a of an existing supernet."""
     result = None
     for prefix in range(network.prefixlen - 1, 0, -1):
         super_network = network.supernet(new_prefix=prefix)
@@ -114,31 +85,24 @@ def find_existing_supernet(network):
             break
     return result
 
-
 def main():
     parser = argparse.ArgumentParser(
-        description="Supply the names of one of more files used for input. "
-        "If no files are supplied, supernets will process standard input, "
-        "allowing you to pipe input from another programs output. "
-        "Each network must be on its own line and in CIDR format. "
-        "supernets works with both IPv4 and IPv6 addresses.")
-    parser.add_argument('subnetFile', nargs=1)
+        description="Supply the names of one of more files used for input. If no files \
+        are supplied, the application will process standard input, allowing you to \
+        pipe input from another programs output. Each network must be on its own line \
+        and in CIDR (i.e., '216.58.192.0/24') format.")
+    parser.add_argument('subnetFile', nargs=1, help="Path to file containing subnets")
     parser.add_argument('-m', '--maxprefixlen', type=int)
     args = parser.parse_args()
     subnets = args.subnetFile[0]
-
     process_input(subnets)
-
     process_prefixes()
 
     for network in sorted(networks, key=lambda ip: ip.network_address.packed):
-        if args.maxprefixlen:
-            if network.prefixlen < args.maxprefixlen:
-                small_supers = list(network.subnets(new_prefix=16))
-                for new_super in small_supers:
-                    print(new_super)
-            else:
-                print(network)
+        if args.maxprefixlen and network.prefixlen < args.maxprefixlen:
+            small_supers = list(network.subnets(new_prefix=16))
+            for new_super in small_supers:
+                print(new_super)
         else:
             print(network)
 
